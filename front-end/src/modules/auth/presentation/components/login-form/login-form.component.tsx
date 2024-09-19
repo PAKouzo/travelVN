6207f655308bc322aa9b8231a3dd24727a86d846
Hieu <dotrunghieuxk@gmail.com>
2024-09-19 17:11:42 +0700

    build styles base

--- front-end/src/modules/auth/presentation/components/login-form/login-form.component.tsx
+++ front-end/src/modules/auth/presentation/components/login-form/login-form.component.tsx
@@ -1,6 +1,6 @@
 import { LoginPayload } from "@/modules/auth/domain/dto/login.dto";
 import { Button, Form, Input } from "antd";
-
+import styles from './login-form.component.module.scss';
 interface Props {
   onSubmit: (values: LoginPayload) => void
   isLoading: boolean;
@@ -9,37 +9,45 @@ interface Props {
 const LoginForm = ({onSubmit, isLoading}: Props) => {
 
   return (
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
-      initialValues={{ remember: true }}
-      onFinish={onSubmit}
-      disabled={isLoading}
-    >
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[{ type: 'email', required: true, message: "Please input your email!" }]}
+    <div className={styles.container}>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onSubmit}
+        disabled={isLoading}
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              type: "email",
+              required: true,
+              message: "Please input your email!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: "Please input your password!" }]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 export default LoginForm;
